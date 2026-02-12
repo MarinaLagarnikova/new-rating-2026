c27767e5e30ad4c1272255a9822eb814e5763123
MarinaLagarnikova <m.lagarnikova@dvizh.io>
2026-02-12 15:06:53 +0300

    Fix GitHub Pages deployment paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/new-rating-2026/',
+  base: './',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
